fe7ae554159e4c840b3fd239ae97b5ab58f8f11d
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-14 16:09:39 -0600

    add working mvp of quail app

--- routes/quails.js
+++ routes/quails.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('quails');
+  knex.table('quails').select().then(function(quails) {
+    console.log(quails);
+    res.render('quails', {quails: quails});
+  });
 });
 
 router.get('/add', function(request, response) {
@@ -11,7 +15,23 @@ router.get('/add', function(request, response) {
 });
 
 router.post('/add', function(request, response) {
-  response.redirect('/quails');
+  // grab info from post
+  var name = request.body.name;
+  var image = request.body.image;
+  var description = request.body.description;
+  // insert into table
+  knex.table('quails').insert({
+    name: name,
+    image: image,
+    description: description
+  }).then(function(message) {
+    // redirect to /quails async style instead of putting redirect outside of this
+    console.log(message);
+    response.redirect('/quails');
+  }).catch(function(error) {
+    // catch error and if error direct to next middleware in app.js to send error page
+    next(error);
+  });
 });
 
 module.exports = router;
